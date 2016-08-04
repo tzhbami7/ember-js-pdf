7466db92773d4e0d959ab54d792b72e9560bac84
Cheng Zhu <cheng@weteachme.com>
2016-08-04 19:18:06 +1000

    update index.js and blueprints

---
+++ blueprints/ember-js-pdf/index.js
@@ -0,0 +1,9 @@
+module.exports = {
+  description: 'Installation blueprint for ember-js-pdf',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function(options) {
+    return this.addBowerPackageToProject('jspdf-autotable', '2.0.33'));
+  }
+};
--- index.js
+++ index.js
@@ -2,5 +2,11 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-js-pdf'
+  name: 'ember-js-pdf',
+  included: function(app) {
+    this._super.included.apply(this, arguments);
+
+    app.import(app.bowerDirectory + '/jspdf/dist/jspdf.min.js');
+    app.import(app.bowerDirectory + '/jspdf-autotable/dist/jspdf.plugin.autotable.js');
+  }
 };
